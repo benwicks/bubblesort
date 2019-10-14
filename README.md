2d89f3b64408c66d3707a296ea1ceb34d92ef8c2
Ben Wicks <benjaminlwicks@gmail.com>
2019-10-14 11:12:45 -0500

    Implement bubble sort in Kotlin and add CI status image to README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bubblesort
+# bubblesort [![Build Status](https://travis-ci.com/benwicks/bubblesort.svg?branch=master)](https://travis-ci.com/benwicks/bubblesort)
 Bubble Sort implemented in Kotlin with tests
--- src/main/kotlin/com/benwicks/BubbleSorter.kt
+++ src/main/kotlin/com/benwicks/BubbleSorter.kt
@@ -2,6 +2,16 @@ package com.benwicks
 
 object BubbleSorter {
     fun <T : Comparable<T>> sort(list: List<T>): List<T> {
-        TODO("Implement Bubble Sort")
+        val sortedList = list.toMutableList()
+        for (pass in 0 until (list.size - 1)) {
+            for (currentPosition in 0 until (sortedList.size - pass - 1)) {
+                if (sortedList[currentPosition] > sortedList[currentPosition + 1]) {
+                    val temporaryItem = sortedList[currentPosition]
+                    sortedList[currentPosition] = sortedList[currentPosition + 1]
+                    sortedList[currentPosition + 1] = temporaryItem
+                }
+            }
+        }
+        return sortedList
     }
 }
